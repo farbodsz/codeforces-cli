cf9b829b8fca9d5bf1dfce97a6a42d47e5979b3e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-05-02 01:31:56 +0100

    Display user rank and prettify user cmd output

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,14 @@
 
 module Main where
 
+import Codeforces.Rank
 import Codeforces.User
 
 import Commands
+
+import Control.Monad.Extra
+
+import Data.List (intercalate)
 
 --------------------------------------------------------------------------------
 
@@ -17,6 +22,9 @@ main = do
 
 --------------------------------------------------------------------------------
 
+indent :: String
+indent = replicate 6 ' '
+
 userInfo :: Handle -> IO ()
 userInfo h = do
     usr <- getUser h
@@ -25,6 +33,30 @@ userInfo h = do
         Right u -> printUser u
 
 printUser :: User -> IO ()
-printUser = print
+printUser u = do
+    let rank = getRank (rating u)
+
+    putStrLn ""
+    putStrLn $ concat [indent, rankName rank, " ", handle u]
+    whenJust (sequenceA [firstName u, lastName u])
+        $ \ns -> putStrLn $ indent ++ unwords ns
+
+    printRatings u
+    printPlace u
+    putStrLn ""
+
+printRatings :: User -> IO ()
+printRatings u = do
+    let currRating = rating u
+    let bestRating = maxRating u
+    putStrLn ""
+    putStrLn $ indent ++ "Rating:       " ++ show currRating
+    putStrLn $ indent ++ "              (max: " ++ show bestRating ++ ")"
+
+printPlace :: User -> IO ()
+printPlace u = do
+    whenJust (sequenceA [city u, country u]) $ \xs ->
+        putStrLn $ indent ++ "City:         " ++ intercalate ", " xs
+    whenJust (organization u) $ \o -> putStrLn $ indent ++ "Organisation: " ++ o
 
 --------------------------------------------------------------------------------
---
+++ src/Codeforces/Rank.hs
@@ -0,0 +1,33 @@
+module Codeforces.Rank where
+
+data RankColor = Gray | Green | Cyan | Blue | Violet | Orange | Red
+
+type RatingBounds = (Int, Int)
+
+data Rank = Rank
+    { rankName     :: String
+    , rankColor    :: RankColor
+    , ratingBounds :: RatingBounds
+    }
+
+-- | `ranks` is list of all Codeforces ranks
+ranks :: [Rank]
+ranks =
+    [ Rank "Newbie"                    Gray   (0   , 1199)
+    , Rank "Pupil"                     Green  (1200, 1399)
+    , Rank "Specialist"                Cyan   (1400, 1599)
+    , Rank "Expert"                    Blue   (1600, 1899)
+    , Rank "Candidate Master"          Violet (1900, 2199)
+    , Rank "Master"                    Orange (2200, 2299)
+    , Rank "International Master"      Orange (2300, 2399)
+    , Rank "Grandmaster"               Red    (2400, 2599)
+    , Rank "International Grandmaster" Red    (2600, 2899)
+    , Rank "Legendary Grandmaster"     Red    (2900, 9999)
+    ]
+
+-- | `getRank` @rating@ returns the @Rank@ that matches the user's @rating@
+getRank :: Int -> Rank
+getRank x = head $ filter withinRankBounds ranks
+  where
+    withinRankBounds r =
+        let bounds = ratingBounds r in x >= fst bounds && x <= snd bounds
