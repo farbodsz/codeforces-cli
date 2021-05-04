3ee1bcb01e0aaff3e133c97346714583b43d4d2e
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-05-04 15:19:49 +0100

    Fix error in rank bounds of Candidate Master

--- src/Codeforces/Rank.hs
+++ src/Codeforces/Rank.hs
@@ -19,8 +19,8 @@ ranks =
     , Rank "Pupil"                     Green  (1200, 1399)
     , Rank "Specialist"                Cyan   (1400, 1599)
     , Rank "Expert"                    Blue   (1600, 1899)
-    , Rank "Candidate Master"          Violet (1900, 2199)
-    , Rank "Master"                    Orange (2200, 2299)
+    , Rank "Candidate Master"          Violet (1900, 2099)
+    , Rank "Master"                    Orange (2100, 2299)
     , Rank "International Master"      Orange (2300, 2399)
     , Rank "Grandmaster"               Red    (2400, 2599)
     , Rank "International Grandmaster" Red    (2600, 2899)
@@ -31,5 +31,4 @@ ranks =
 getRank :: Int -> Rank
 getRank x = head $ filter withinRankBounds ranks
   where
-    withinRankBounds r =
-        let bounds = rankBounds r in x >= fst bounds && x <= snd bounds
+    withinRankBounds Rank {..} = fst rankBounds <= x && x <= snd rankBounds
