ccd9f56d6e93c374c52ab1015900453723db5e75
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-06-09 01:04:33 +0100

    Show errors and last update time in watch command

--- app/Watcher.hs
+++ app/Watcher.hs
@@ -2,14 +2,19 @@
 
 module Watcher where
 
-import Codeforces
+import Codeforces hiding (RankColor(..))
 
 import Control.Concurrent
 import Control.Monad (zipWithM_)
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.State
 
+import Data.Maybe (catMaybes)
+import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Time
+
+import Format
 
 import System.Console.ANSI
 
@@ -21,23 +26,29 @@ import Table
 -- false, otherwise 'watchTable' watches it.
 handleWatch :: Bool -> IO (Either CodeforcesError Table) -> IO ()
 handleWatch False m = m >>= either (putStrLn . showE) (mapM_ T.putStrLn)
-handleWatch True  m = evalStateT (watchTable 5 m) []
+handleWatch True  m = clearScreen >> evalStateT (watchTable 5 m) ([], Nothing)
 
 -- | 'watchTable' @delaySecs m@ runs computation @m@ every @delaySecs@ amount of
 -- seconds. The terminal output from @m@ is changed if the next run of @m@
 -- yields a different result.
-watchTable :: Int -> IO (Either CodeforcesError Table) -> StateT Table IO ()
+watchTable
+    :: Int                                  -- ^ Delay, in seconds.
+    -> IO (Either CodeforcesError Table)    -- ^ Fetches an updated table.
+    -> StateT (Table, Maybe UTCTime) IO ()  -- ^ Table and last update time.
 watchTable delaySecs m = do
-    currTable <- get
-    result    <- lift m
+    (oldTable, lastUpdate) <- get
+    now                    <- lift getCurrentTime
 
-    case result of
-        Left  e         -> lift $ putStrLn $ showE e
-        Right nextTable -> if currTable == nextTable
-            then pure ()
-            else do
-                lift $ updateTableOutput currTable nextTable
-                put nextTable
+    let currUpdate = Just now
+
+    -- Fetch updated table and add information rows
+    (newTable, updateTime) <- lift m >>= \case
+        Left  e -> pure (addInfo oldTable (Just e) now lastUpdate, lastUpdate)
+        Right t -> pure (addInfo t Nothing now currUpdate, currUpdate)
+
+    -- Print/store currently displayed table with the time it was retrieved
+    put (newTable, updateTime)
+    lift $ updateTableOutput oldTable newTable
 
     lift $ threadDelay (delaySecs * 1000000)
     watchTable delaySecs m
@@ -55,6 +66,35 @@ updateTableOutput currTable nextTable = do
     updateRow r1 r2
         | r1 == r2  = cursorDownLine 1
         | otherwise = T.putStrLn r2
+
+--------------------------------------------------------------------------------
+
+-- | Takes a table with a potential error message and last successful update
+-- time, and returns the table with extra rows for various messages.
+--
+-- When tables are displayed in watch mode, these extra rows are placed before
+-- the table:
+--  * The first displays a message for how to exit watch mode.
+--  * The second row displays errors if there are any and the last update time.
+--  * The third row is blank.
+--
+addInfo
+    :: Table                  -- ^ Table to modify.
+    -> Maybe CodeforcesError  -- ^ Error message, if exists.
+    -> UTCTime                -- ^ Current system time.
+    -> Maybe UTCTime          -- ^ Last successful update time, if any.
+    -> Table
+addInfo table me now lastUpdate =
+    colored Cyan "Watching for updates. Press CTRL+c to exit."
+        : statusMsg
+        : ""
+        : table
+  where
+    statusMsg = T.concat $ catMaybes [errorMsg, updateMsg]
+
+    errorMsg  = colored Red . (<> " ") . T.pack . showE <$> me
+    updateMsg = T.concat <$> sequenceA
+        [Just "Last update: ", fmtDiffTime <$> (diffUTCTime now <$> lastUpdate)]
 
 --------------------------------------------------------------------------------
 
